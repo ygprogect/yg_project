7d6c8078dd2182389f8ab88f1db418475481bfd1
RAMI <ygyemengas@gmail.com>
2023-06-23 01:04:58 +0300

    forth_editing

---
+++ app/src/main/java/com/example/yg/notification_sreen.java
@@ -0,0 +1,14 @@
+package com.example.yg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class notification_sreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_sreen);
+    }
+}
