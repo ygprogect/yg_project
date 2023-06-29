e8ce6b5b64c87e96e94472229419d4b45d06a1b0
GamalGamil66 <alnhmy66@gmail.com>
2023-06-29 09:19:53 +0300

    no_thing

--- app/src/main/java/com/example/yg/Server/URLs.java
+++ app/src/main/java/com/example/yg/Server/URLs.java
@@ -2,6 +2,7 @@ package com.example.yg.Server;
 
 public class URLs {
 //    192.168.43.128:8080\api\citizen\login
+//    192.168.43.128:8080\api\citizen\login
     public static final String URL = "http://192.168.43.128:8080/";
     public static final String HOME = URL+"api";
     public static final String CITIZEN_LOGIN = HOME+"/citizen/login";
