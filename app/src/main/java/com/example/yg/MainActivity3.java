2f89d26ad9580902f4619a1f6ee80623b892e398
RAMI <ygyemengas@gmail.com>
2023-06-30 21:53:32 +0300

    forth_editing3

--- app/src/main/java/com/example/yg/MainActivity3.java
+++ app/src/main/java/com/example/yg/MainActivity3.java
@@ -21,28 +21,28 @@ public class MainActivity3 extends AppCompatActivity  {
         setContentView(R.layout.activity_main3);
 
         bottomNavigationView = findViewById(R.id.bottom_nav3);
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_container , new orderFragment()).commit();
-        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container3 , new orderFragment()).commit();
+        bottomNavigationView.setSelectedItemId(R.id.nav_home3);
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment = null;
                 switch (item.getItemId()){
-                    case R.id.nav_setting:
+                    case R.id.nav_setting3:
                         fragment = new settingFragment();
                         break;
 
-                    case R.id.nav_add:
+                    case R.id.nav_add3:
                         fragment = new myorderFragment();
                         break;
 
 
-                    case R.id.nav_home:
+                    case R.id.nav_home3:
                         fragment = new orderFragment();
                         break;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.main_container , fragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container3 , fragment).commit();
 
                 return true;
             }
