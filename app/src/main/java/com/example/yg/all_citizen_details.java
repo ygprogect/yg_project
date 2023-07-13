b39070546be767e90d31a0a058a2a29f26d83c15
ahmed <ahmedalmosawawww2000@gmail.com>
2023-07-14 02:01:14 +0300

    ..

---
+++ app/src/main/java/com/example/yg/all_citizen_details.java
@@ -0,0 +1,14 @@
+package com.example.yg;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class all_citizen_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_citizen_details);
+    }
+}
