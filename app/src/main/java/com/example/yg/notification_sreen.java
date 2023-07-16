a9f10287630344bd414701a48a07c1d1bb58875a
RAMI <ygyemengas@gmail.com>
2023-07-16 04:12:29 +0300

    forth_editing4.7

--- app/src/main/java/com/example/yg/notification_sreen.java
+++ app/src/main/java/com/example/yg/notification_sreen.java
@@ -3,12 +3,21 @@ package com.example.yg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class notification_sreen extends AppCompatActivity {
-
+private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_sreen);
+        imageView=findViewById(R.id.notification_exit);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
